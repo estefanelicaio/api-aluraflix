054be538760c04fd8b39dbd4d2249563fb44f2c6
Caio Estefaneli <caioestefaneli@gmail.com>
2022-09-25 16:40:10 -0300

    Implementado CRUD de videos

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\VideoRequest;
 use App\Models\Video;
-use Illuminate\Http\Request;
 
 class VideoController extends Controller
 {
@@ -15,7 +15,7 @@ class VideoController extends Controller
      */
     public function index()
     {
-        //
+        return Video::all();
     }
 
     /**
@@ -24,42 +24,61 @@ class VideoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(VideoRequest $request)
     {
-        //
+        $video = Video::create($request->all());
+
+        return response()->json($video, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Video  $video
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Video $video)
+    public function show(int $id)
     {
-        //
+        $video = Video::find($id);
+
+        if(!$video) {
+            return response()->json(['mensagem' => 'Vídeo não encontrado'], 404);
+        }
+
+        return $video;
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Video  $video
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Video $video)
+    public function update(VideoRequest $request, int $id)
     {
-        //
+        $video = Video::find($id);
+
+        if(!$video) {
+            return response()->json(['mensagem' => 'Vídeo não encontrado'], 404);
+        }
+
+        $video->update($request->all());
+        return $video;
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Video  $video
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Video $video)
+    public function destroy(int $id)
     {
-        //
+        if(Video::destroy($id)) {
+            return response()->json(['mensagem' => 'Video excluído com sucesso']);
+        }
+
+        return response()->json(['mensagem' => 'Vídeo não encontrado'], 404);
     }
 }
