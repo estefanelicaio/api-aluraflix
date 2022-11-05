c8389ca4d2e6aec64dcb7de1e5a522ebc468319e
Caio Estefaneli <caioestefaneli@gmail.com>
2022-11-05 19:00:29 -0300

    Criado factory para gerar vídeos

---
+++ database/factories/VideoFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Video>
+ */
+class VideoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(20),
+            'description' => $this->faker->text(100),
+            'url' => $this->faker->url(),
+            'category_id' => $this->faker->numberBetween(1, Category::count())
+        ];
+    }
+}
