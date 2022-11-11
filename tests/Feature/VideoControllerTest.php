5d6ee9264f8fd20ce846f7f3ed0001f05d4014a6
Caio Estefaneli <caioestefaneli@gmail.com>
2022-11-10 22:29:07 -0300

    Alterado nome da classe de teste

--- tests/Feature/VideoControllerTest.php
+++ tests/Feature/VideoControllerTest.php
@@ -4,10 +4,9 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class VideoTest extends TestCase
+class VideoControllerTest extends TestCase
 {
     use RefreshDatabase;
 
